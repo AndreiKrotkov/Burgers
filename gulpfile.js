ce031341c8eaedd380a89a01747dcc0b7cd1fd2f
Andrei Krotkov <wottanks2008@yandex.ru>
2019-09-19 19:01:25 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,157 @@
+const { src, dest, task, series, watch, parallel} = require('gulp');
+const rm = require('gulp-rm');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+const px2rem = require('gulp-smile-px2rem');
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const svgo = require('gulp-svgo');
+const svgSprite = require('gulp-svg-sprite');
+// const {SRC_PATH, DIST_PATH, STYLE_LIBS, JS_LIBS} = require('./gulp.config');
+const gulpif = require('gulp-if');
+
+const env = process.env.NODE_ENV;
+
+sass.compiler = require('node-sass');
+
+task('clean', () => {
+  console.log(env);
+    return src('dist/**/*', { read: false }).pipe(rm());
+  });
+
+task("copy:html", () => {
+    return src('app/*.html')
+      .pipe(dest('dist'))
+      .pipe(reload({stream: true}));
+});
+
+task("copy:img", () => {
+  return src('app/img/**/*.+(png|jpg|jpeg)')
+    .pipe(dest('dist/images/img'))
+    .pipe(reload({stream: true}));
+});
+
+task("fonts", () => {
+  return src('app/fonts/**/*')
+  .pipe(dest('dist/fonts'))
+});
+
+task("svg", () => {
+  return src('app/img/sprite/*.svg')
+  .pipe(dest('dist/sprite'))
+});
+task("svgIcon", () => {
+  return src('app/img/icons/*.svg')
+  .pipe(dest('dist/images/icon'))
+});
+
+task("fullPageCss", () => {
+  return src('app/fullpagecss/*.css')
+  .pipe(dest('dist'))
+});
+task("fullPageJs", () => {
+  return src('app/fullpagejs/*.js')
+  .pipe(dest('dist'))
+});
+
+
+const styles = [
+  'node_modules/normalize.css/normalize.css',
+  'app/scss/main.scss'
+];
+
+task('styles', () => {
+    return src(styles)
+    .pipe(gulpif(env === 'dev', sourcemaps.init()))
+    .pipe(concat('main.min.scss'))
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(px2rem())
+    .pipe(gulpif(env === 'prod', autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+  })))
+    .pipe(gulpif(env === 'prod', gcmq()))
+    .pipe(gulpif(env === 'prod', cleanCSS()))
+    .pipe(gulpif(env === 'dev', sourcemaps.write()))
+    .pipe(dest('dist'))
+    .pipe(reload({ stream: true }));
+})
+
+const libs = [
+  'node_modules/jquery/dist/jquery.js',
+  'app/js/*.js'
+ ];
+
+task('script', () => {
+  return src(libs)
+  .pipe(gulpif(env === 'dev', sourcemaps.init()))
+  .pipe(concat('main.min.js', { newLine: ';' }))
+  .pipe(gulpif(env === 'prod', babel({
+    presets: ['@babel/env']
+  })))
+  .pipe(gulpif(env === 'prod', uglify()))
+  .pipe(gulpif(env === 'dev', sourcemaps.write()))
+  .pipe(dest('dist'))
+  .pipe(reload({ stream: true }));
+})
+
+// task('icons', () => {
+//   return src('app/img/icons/*.svg')
+//   .pipe(svgo({
+//     plugins: [
+//       {
+//         removeAttrs: {
+//           attrs: '(fill|stroke|style|width|height|data.*)'
+//         }
+//       }
+//     ]
+//   }))
+//   .pipe(svgSprite({
+//     mode: {
+//       symbol: {
+//         sprite: '../sprite.svg'
+//       }
+//     }
+//   }))
+//   .pipe(dest('dist/images/icons'));
+// })
+
+task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        },
+        open: true
+    });
+});
+
+task('watch', () => {
+  watch('app/scss/**/*.scss', series('styles'));
+  watch('app/**/*.html', series('copy:html'));
+  watch('app/js/*.js', series('script'));
+  // watch('app/img/icons/*.svg', series('icons'));
+ });
+  
+  
+ task('default',
+  series(
+    'clean', 
+    parallel('copy:html', 'copy:img', 'fullPageCss', 'fullPageJs', 'fonts', "svg", 'svgIcon', 'styles', 'script'),
+    parallel('watch', 'server')
+  )
+ );
+  // 'icons'
+ task('build',
+  series(
+    'clean',
+    parallel('copy:html', 'copy:img', 'fullPageCss', 'fullPageJs', 'fonts', "svg", 'svgIcon', 'styles', 'script',))
+ );
+// 'icons'
